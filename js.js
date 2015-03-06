efd99d32ddffb7a6c4744be310bc95a8edccbbd1
Toadsoup <toadsoup@gmail.com>
2015-03-05 21:17:59 -0600

    Broke out function and added number lib

--- js.js
+++ js.js
@@ -16,7 +16,7 @@ var checkBand = function (color) {
     return colorCodes[color.toLowerCase()];
   }
 
-var checkTolerance =  function(color) {
+var checkTolerance = function(color) {
   console.log(color);
   var toleranceCodes = {
       'brown' : 1,
@@ -33,46 +33,54 @@ var checkTolerance =  function(color) {
     return toleranceCodes[color.toLowerCase()];
 }
 
+var checkMultiplier = function (multiplier) {
+  var temp = 1;
+  if( multiplier >= 0 ){
+    for(var x = 0; x < multiplier; x++){
+      temp *= 10;                              
+    }
+    return temp;
+  }
+}
 
-
-  $(document).ready( function () {
-    var resistance = 0;
-    var band1 = '';
-    var band2 = '';
-    var band3 = '';
-    var tolerance = 'none';
-    var band1val = 0;
-    var band2val = 0;
-    var bandmult = 1;
+$(document).ready( function () {
+  var resistance = 0;
+  var band1 = '';
+  var band2 = '';
+  var band3 = '';
+  var tolerance = 'none';
+  var band1val = 0;
+  var band2val = 0;
+  var bandmult = 1;
             
-    $('.band').on('click', function(e){
-      if($(this).hasClass("band1")){
-        band1 = $(event.target).text();
-        $('#b1').css('background-color', $(event.target).text());
-        band1val = checkBand($(event.target).text());
-      } else if($(this).hasClass("band2")){
-        $('#b2').css('background-color', $(event.target).text());
-        band2 = $(event.target).text();    
-        band2val = checkBand($(event.target).text());
-      } else if($(this).hasClass("band3")){
-        $('#b3').css('background-color', $(event.target).text());
-        band3 = $(event.target).text();     
-        bandmult = checkBand($(event.target).text());
-        var temp = 1;
-        if(bandmult == 0 || bandmult >=1 ){
-          for(var x = 0; x < bandmult; x++){
-            temp *= 10;                              
-          }
-          bandmult = temp;
-        }
-      } else if($(this).hasClass("band4")){
-        $('#b4').css('background-color', $(event.target).text());
-        tolerance = $(event.target).text();                        
-      }
+  $('.band').on('click', function(e){
+
+    if($(this).hasClass("band1")){
+      band1 = $(event.target).text();
+      $('#b1').css('background-color', $(event.target).text());
+      band1val = checkBand($(event.target).text());
+      
+    } else if($(this).hasClass("band2")){
+      $('#b2').css('background-color', $(event.target).text());
+      band2 = $(event.target).text();    
+      band2val = checkBand($(event.target).text());
+      
+    } else if($(this).hasClass("band3")){
+      $('#b3').css('background-color', $(event.target).text());
+      band3 = $(event.target).text();     
+      bandmult = checkMultiplier( checkBand($(event.target).text() ) );       
+
+    } else if($(this).hasClass("band4")){
+      $('#b4').css('background-color', $(event.target).text());
+      tolerance = $(event.target).text();                        
+    }
+
       if (band1val != 0 || band2val != 0){
+        resistance = (((band1val * 10)+band2val)*bandmult);
+        var string = numeral(resistance).format('00.0 a');
         $('#valueout').html('<h1>Resistance is ' 
-          + (((band1val * 10)+band2val)*bandmult) + " Ohm" 
-          + ((((band1val * 10)+band2val)*bandmult) > 1 ? "s" : "")
+          + string + " Ohm" 
+          + ( resistance > 1 ? "s" : "")
           + " ±"
           + checkTolerance(tolerance) 
           + "%.</h1>"
